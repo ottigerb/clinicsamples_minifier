5c14c6dd48472bcf97779b12c9199ac495930509
Beat Ottiger <ottigerb@gmail.com>
2020-04-02 11:33:37 +0200

    Working Version

--- src/components/Optimizer.vue
+++ src/components/Optimizer.vue
@@ -7,9 +7,14 @@
         <div class="text--primary">Original - Clinic Sample</div>
         <vue-json-editor v-model="clinic_sample" :mode="mode" :show-btns="show_buttons" :exapndedOnStart="expanded" @json-change="onJsonChange"></vue-json-editor>
       </v-card-text>
-      <v-card-actions v-if="valid_clinic_sample">
-        <v-btn text @click="do_optimize">Optimize Now</v-btn>
-      </v-card-actions>
+      <div v-if="valid_clinic_sample">
+        <v-card-text>
+          <v-alert type="success" v-text="valid_clinic_sample_text"></v-alert>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn text @click="do_optimize">Optimize Now</v-btn>
+        </v-card-actions>
+      </div>
       <div v-else>
         <v-card-text>
           <v-alert type="info" v-text="valid_clinic_sample_text"></v-alert>
@@ -17,14 +22,17 @@
       </div>
     </v-card>
 
-    <v-card style="margin-top:24px;">
-      <v-card-text v-if="optimized">
+    <v-card style="margin-top:24px;" v-if="optimized">
+      <v-card-text>
         <div class="overline">RESULT:</div>
         <p class="display-1 font-weight-regular text--primary" v-text="win_size"></p>
         <div class="text--primary">Optimized - Clinic Sample</div>
 
         <vue-json-editor v-model="optimized_clinic_sample" :mode="mode" :show-btns="show_buttons" :exapndedOnStart="expanded" @json-change="onJsonChange"></vue-json-editor>
       </v-card-text>
+      <v-card-actions>
+        <v-btn text @click="save_file">Download</v-btn>
+      </v-card-actions>
     </v-card>
   </div>
 </template>
@@ -58,14 +66,6 @@ export default {
         return null;
       }
     },
-    current_variables() {
-      try {
-        return this.clinic_sample.variables;
-      } catch (e) {
-        console.error("current_variables", e);
-        return null;
-      }
-    },
     win_size() {
       try {
         var start = Math.round(JSON.stringify(this.clinic_sample).length / 1000);
@@ -79,17 +79,25 @@ export default {
     }
   },
   methods: {
-    load_cs: function(sample) {
-      this.clinic_sample = this.$store.state.cs.bscl;
-      console.log(sample);
+    save_file: function() {
+      const data = JSON.stringify(this.optimized_clinic_sample);
+      const blob = new Blob([data], {type: 'text/plain'})
+      const e = document.createEvent('MouseEvents'),
+      a = document.createElement('a');
+      a.download = "clinicsample_optimized.json";
+      a.href = window.URL.createObjectURL(blob);
+      a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
+      e.initEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+      a.dispatchEvent(e);
     },
     round_one: function(number) {
       return Math.round(number * 10) / 10;
     },
-    clean_data_array: function(array_item) {
-      array_item.forEach(element => {
+    clean_data_array: function(value) {
+      var array_item = value.slice();
+      array_item.forEach(function (element) {
         if (Array.isArray(element)) {
-          this.clean_data_array(element);  
+          element = this.clean_data_array(element);  
         } else {
           if (element !== null) {
             try {
@@ -101,23 +109,20 @@ export default {
               };
               if ('statistics' in element) {
                 
-              //   // element.statistics
-              //   this.clinic_sample.variables.forEach(variable => {
-              //     const stat = Object.assign({}, element.statistics[variable]);
-
-              //     // Loop Object 
-              //     const keys = Object.keys(stat);
-              //     console.log(keys);
-              //     keys.forEach(key => {
-              //       stat[key] = this.round_one(stat[key]);
-              //       //stat[key] = null;
-              //     });
-              //     
-              //     // Save
-              //     console.error('===>', stat);
-              //     element.statistics[variable] = stat;
-              //   });
-
+                // element.statistics
+                const vars = this.clinic_sample.variables.slice();
+                vars.forEach(function (variable) {
+                   const stat = Object.assign({}, element.statistics[variable]);
+                    // Loop Object 
+                   const keys = Object.keys(stat);
+                   // console.log(keys);
+                   keys.forEach(function (key) {
+                     stat[key] = this.round_one(stat[key]);
+                   }.bind(this));
+                   // Save
+                   // console.error('===>', stat);
+                   element.statistics[variable] = stat;
+                 }.bind(this));
               };
                 
             } catch (e) {
@@ -125,26 +130,12 @@ export default {
             };
           };
         };
-      });
-    },
-    clean_dimensions_array: function(array_item) {
-      array_item.forEach(element => {
-        if (Array.isArray(element)) {
-          this.clean_dimensions_array(element);  
-        } else {
-          if (element !== null) {
-            if ('scores' in element) {
-              delete element.scores;  
-            };
-            if ('patients' in element) {
-              delete element.patients;  
-            };
-          };
-        };
-      }).bind(this);
+      }.bind(this));
+      return array_item;
     },
     do_optimize: function() {
-      var cs = Object.assign({}, this.clinic_sample);
+      // Deep copy: This one safely copies deeply nested objects/arrays!
+      var cs = JSON.parse(JSON.stringify(this.clinic_sample));
       console.log("-- do_optimize", cs);
 
       // Delete Unneeded stuff
@@ -156,16 +147,7 @@ export default {
       };
 
       // Remove Patient & Scores
-      this.clean_data_array(cs.data); 
-      // cs.data.forEach(element => {
-      //   console.log('element', element);
-      //   if (Array.isArray(element)) {
-      //      
-      //   };
-      // });
-
-      // round to one decimal
-      // this.round_one();
+      cs.data = this.clean_data_array(cs.data); 
 
       // Set to Optimized
       this.optimized_clinic_sample = cs;
@@ -174,6 +156,7 @@ export default {
     do_validate: function(cs) {
       console.log("-- do_validate", cs);
       var return_bool = false;
+      this.optimized = false;
       if ('date' in cs) {
         if ('dimensions' in cs) {
           if (cs.dimensions.length > 0) {
@@ -183,6 +166,7 @@ export default {
                   if (cs.data.length > 0) {
                     return_bool = true;
                     this.mode = 'tree';
+                    this.valid_clinic_sample_text = 'Your clinic sample is valid. You can now Optimize it:'
                   };
                 };
               };
@@ -193,7 +177,7 @@ export default {
       this.valid_clinic_sample = return_bool;
     },
     onJsonChange (value) {
-        console.log('value:', value);
+        // console.log('value:', value);
         this.do_validate(value);
     }
   },
